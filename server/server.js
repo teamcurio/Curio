7f9ca53ab9f4a1d8cadc26a5bd101522401e9846
Trevor Carr <trevor.a.carr@gmail.com>
2021-04-28 22:21:54 -0400

    begin implementation of graphQL

--- server/server.js
+++ server/server.js
@@ -1,28 +1,25 @@
 const path = require('path');
 const express = require('express');
+// const graphqlHTTP = require('express-graphql');
+// const schema = require('./models/graphqlSchema');
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
 const app = express();
 
+// app.use(
+//   '/graphql',
+//   graphqlHTTP({
+//     schema,
+//     graphiql: true,
+//   }),
+// );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-
-
-app.use(express.static(path.join(__dirname, '../build')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
-});
-
-
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../src/index.html"));
-//   });
 app.use(express.static(path.join(__dirname, '../src')));
-
 
 
 //global error handler
