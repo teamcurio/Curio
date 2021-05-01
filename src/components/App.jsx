65dac09753247df49a634637ac69655064a4db10
Damon Alfaro <djdalfaro@protonmail.com>
2021-05-01 15:42:06 -0600

    corrected deleted lines in app.jsx coauthor: Anika Mustafiz munikamustafiz89@gmail.com coauthor: Trevor Carr trevor.a.carr@gmail.com coauthor: Chandni Patel chandnip6@gmail.com coauthor: Steve Hong stevehong423@gmail.com

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,23 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import NavBar from './NavBar';
 import Images from './Images';
+import SignIn from './SignInPage';
+import { Switch, Route } from 'react-router-dom';
 
 const client = new ApolloClient({
   uri: 'http://localhost:8080/graphql'
 });
-
+class App extends Component {
   render() {
     return(
     <ApolloProvider client={client}>
       {/* // navbar component */}
-      <NavBar/>
+        <NavBar />
+        <div className='router'>
+          <Switch>
+            <Route exact path="/signin" component={SignIn} />
+          </Switch>
+        </div>
       <Images />
       {/* // display component */}
       {/* // description component */}
