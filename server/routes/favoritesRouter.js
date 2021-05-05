247432fe5cca8729ed30ec8575cece510de6d11d
Chandni Patel <chandnip6@gmail.com>
2021-05-04 21:30:23 -0400

    Add favorites router for add and delete Co-authored-by: Steve Hong <stevehong423@hotmail.com>

--- server/routes/favoritesRouter.js
+++ server/routes/favoritesRouter.js
@@ -9,7 +9,12 @@ router.get("/getFavorites", favoritesController.getFavorites, (req, res) => {
   res.status(200).json(res.locals.favorites);
 });
 
+router.post('/addFavorite', favoritesController.addFavorite, (req, res) => {
+  res.status(200).send('Favorite Saved');
+});
 
-
+router.delete('/deleteFavorite', favoritesController.deleteFavorite, (req, res) => {
+  res.status(200).send('Favorite Deleted')
+});
 
 module.exports = router;
