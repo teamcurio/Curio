5116251240e4b1f903db66325cdb11730b2e8ca6
Chandni Patel <chandnip6@gmail.com>
2021-05-04 21:29:58 -0400

    Add favorites controller for add and delete Co-authored-by: Steve Hong <stevehong423@hotmail.com>

--- server/controllers/favoritesController.js
+++ server/controllers/favoritesController.js
@@ -23,12 +23,47 @@ favoritesController.getFavorites = (req, res, next) => {
         });
 };
 
-favoritesController.addFavorites = (req, res) => {
+favoritesController.addFavorite = (req, res, next) => {
+    const { image_id } = req.body;
+    const user_id = res.locals.user_id;
 
+    const addFaveQuery = `INSERT INTO "Favorites" (user_id, image_id) VALUES ($1, $2) RETURNING *`
+    const values = [user_id, image_id];
+
+    db.query(addFaveQuery, values)
+        .then((data) => {
+            return next();
+        })
+        //TODO: err.constraint?
+        .catch((err) => {
+            return next({
+                message: { err: 'Error adding Favorite to database'}
+            })
+        })
 };
 
-favoritesController.deleteFavorites = (req, res) => {
+favoritesController.deleteFavorite = (req, res, next) => {
+    const { image_id} = req.body;
+    const user_id = res.locals.user_id;
 
+    const deleteFave = `DELETE FROM "Favorites" WHERE user_id=$1 AND image_id=$2 RETURNING *;`
+    const values = [user_id, image_id];
+
+    db.query(deleteFave, values)
+        .then((data) => {
+            console.log(data)
+            if (data.rows[0]) return next();
+            else {
+                return next({
+                    message: { err: 'Favorite does not exist for user.'}
+                });
+            }
+        })
+        .catch((err) => {
+            return next({
+                message: { err: 'Error deleting favorite from database'}
+            })
+        })
 };
 
 module.exports = favoritesController
