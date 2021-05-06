cc9fcc5b053c2d496af9826bb5e886bb6db9c4f7
Trevor Carr <trevor.a.carr@gmail.com>
2021-05-06 15:48:39 -0400

    fix router for ImageContainer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,13 +12,13 @@ import NavBar from './NavBar';
 import { Switch, Route } from 'react-router-dom';
 import ImageContainer from './ImageContainer';
 import favorites from './favorites';
-const arr = [
-  { show: "block", url: "https://source.unsplash.com/WLUHO9A_xik/1440x960" },
-  { show: "none", url: "https://source.unsplash.com/DNE9iZ1Kqzk/1440x960" },
-  { show: "none", url: "https://source.unsplash.com/6ccJQ5qPFvY/1440x960" },
-  { show: "none", url: "https://source.unsplash.com/qTLyiHW1nIc/1440x960" },
-  { show: "none", url: "https://source.unsplash.com/fxX__3GRtsg/1440x960" }
-];
+// const arr = [
+//   { show: "block", url: "https://source.unsplash.com/WLUHO9A_xik/1440x960" },
+//   { show: "none", url: "https://source.unsplash.com/DNE9iZ1Kqzk/1440x960" },
+//   { show: "none", url: "https://source.unsplash.com/6ccJQ5qPFvY/1440x960" },
+//   { show: "none", url: "https://source.unsplash.com/qTLyiHW1nIc/1440x960" },
+//   { show: "none", url: "https://source.unsplash.com/fxX__3GRtsg/1440x960" }
+// ];
 // localStorage.setItem('curioUser', '0001')
 const client = new ApolloClient({
   uri: 'http://localhost:8080/graphql'
@@ -33,7 +33,7 @@ class App extends Component {
           <Switch>
             <Route path='/' exact component={LandingPage} />
             <Route exact path="/login" component={LogIn} />
-            <Route exact path='/images' render={(props) => <ImageContainer {...props} imgArr={arr} />} />
+            <Route exact path='/images' component={ImageContainer} />
             <Route exact path='/favorites' render={(props) => <ImageContainer {...props} imgArr={favorites(localStorage.getItem(curioUser))} />} />
             <Route exact path='/signup' component={SignUp} />
           </Switch>
