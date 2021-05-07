b50e50e6ed875722913a2e1f5abf170cf8ca7cba
stevehong423 <stevehong423@hotmail.com>
2021-05-06 17:42:10 -0700

    testing

---
+++ cypress/integration/init.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Cypress', () => {   
+  it('is working', () => {     
+    expect(true).to.equal(true)   
+  });
+    
+  it('Opens the app', () => {   
+    cy.visit('http://localhost:8080').contains('Curio'); 
+  });
+
+  it('Checks Input', () => {
+    cy.get('input[placeholder="Enter Search Term"]').type('h1');
+  }) 
+
+  it('Search redirects to /images', () => {
+    cy.get('.chakra-input__right-element > a > .chakra-button').click();
+    cy.url().should('contain', '/images');
+  });
+
+});
